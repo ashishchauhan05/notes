197cf057e1fdf56f129e923f72317b4af05dea42
Ashish Chauhan <ashchauhan77@gmail.com>
2018-04-05 16:34:41 +0530

    sql file

--- resources/views/emails/signup_email.blade.php
+++
@@ -1,5 +0,0 @@
-Hi!<br><br>
-Thanks for creating an account with the Glamstudios.Please follow the link below to verify your email address<br><br>
-{{ URL::to('signup/verify/' . $data['confirmation_code']) }}.<br><br>
-Team – Ministry<br><br>
-Call on 8010-000-444 for booking related assistance.<br><br>
